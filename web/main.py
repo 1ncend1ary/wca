7f0581720218ff4571a63eadc5533f32a0f4956e
a <alexseliverstov@yahoo.com>
2020-02-23 15:07:03 +0300

    Create a simple webapp

---
+++ web/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/contacts")
+def contacts():
+    return render_template("contacts.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
