e79c3cb65ffc2c54216ae7e8f4e2500a7735b0fa
a <alexseliverstov@yahoo.com>
2020-02-25 10:42:02 +0300

    Update webapp to use redis

--- web/main.py
+++ web/main.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template
+import time
+import redis
 
 app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    count = get_hit_count()
+    return render_template("index.html", count=count)
 
 @app.route("/contacts")
 def contacts():
     return render_template("contacts.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
