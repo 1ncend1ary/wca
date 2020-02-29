92e4d7e41c02b681e14a241077ce000d7b908881
a <alexseliverstov@yahoo.com>
2020-02-29 12:00:49 +0300

    Update web to match new database categories

--- web/main.py
+++ web/main.py
@@ -4,26 +4,13 @@ import redis
 import database
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
 
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
-
-
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    count = get_hit_count()
+    #count = get_hit_count()
     element = database.fetch_new_entry()
-    return render_template("index.html", word=element['Word'], image=element["URL"])
+    return render_template("index.html", word=element['category name'], image=None)
 
 
 @app.route("/contacts")
