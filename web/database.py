98cef656816613d6c81b5fab656a19f013bca3d2
a <alexseliverstov@yahoo.com>
2020-03-04 15:26:27 +0300

    Add database write

--- web/database.py
+++ web/database.py
@@ -8,8 +8,17 @@ categories = db[secret.collection_name]
 
 
 def fetch_new_entry():
-    results = list(categories.aggregate([{'$sample': {'size': 1}}]))
+    results = list(categories.aggregate([{'$match': {"annotation": {"$exists": False}}},
+                                         {'$sample': {'size': 1}}]))
     if len(results) != 1:
         return None
     else:
         return results[0]
+
+
+def write_interests(category_id, interests):
+    # category = categories.find_one({'category_id': category_id})
+    categories.update_one({'category_id': category_id},
+                          {'$set': {
+                              'annotation': ', '.join(interests)
+                          }}, upsert=False)
