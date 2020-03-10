249297a736f3ee084469d0f7b142636a8706a168
incendiary <alexseliverstov@yahoo.com>
2020-03-10 18:23:16 +0300

    Add useful scripts

---
+++ bin/readme.txt
@@ -0,0 +1,2 @@
+archive.sh -- a script to create a .tar archive of secret files
+unarchive.sh -- a script to unpack a .tar archive [from the current project root: ~/../wca/secrets.tar.gz] of secret files
---
+++ bin/unarchive.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+current_path=$(pwd)
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" || echo "cd command failed" ; pwd -P )
+cd "$parent_path" || echo "cd command failed"
+
+cd ..
+tar -xvzf secrets.tar.gz
+# -x: Extract an archive.
+# -z: Compress the archive with gzip.
+# -v: Display progress in the terminal while creating the archive, also known as “verbose” mode. The v is always optional in these commands, but it’s helpful.
+# -f: Allows you to specify the filename of the archive.
+
+cd "$current_path" || echo "cd command failed"
