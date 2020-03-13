f5a4ed34bf145c3a0600296b57927d03a8ecc86f
incendiary <alexseliverstov@yahoo.com>
2020-03-13 21:59:45 +0300

    Fix pictures not showing

--- web/scraper.py
+++ web/scraper.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 _google_url = 'https://www.google.com/search?q={}&client=opera&hs=cTQ&source=lnms&tbm=isch&sa=X&ved' \
               '=0ahUKEwig3LOx4PzKAhWGFywKHZyZAAgQ_AUIBygB&biw=1920&bih=982 '
 
-_imagenet_url = 'http://www.image-net.org/api/text/imagenet.synset.geturls?wnid='
+_imagenet_url = 'http://www.image-net.org/api/text/imagenet.synset.geturls?wnid={}'
 
 
 def g_supply_images(keyword):
@@ -18,7 +18,7 @@ def g_supply_images(keyword):
     Get list of supplementary images from google search by keyword
     """
     url = _google_url.format(keyword)
-    bs = BeautifulSoup(fr.get2str(url).text, 'html.parser')
+    bs = BeautifulSoup(fr.get2str(url), 'html.parser')
 
     images = [i.get('src') for i in bs.find_all('img')[:10]]
     print(images[:5])
